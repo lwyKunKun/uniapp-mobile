303f453c2b0cf1749ed77bacedbd6d4ae7cec06b
lwykunKun <458426099@qq.com>
2021-07-16 11:41:30 +0800

    解决scroll-view填满剩下高度,自适应

--- src/pages/backlog/modules/details.vue
+++ src/pages/backlog/modules/details.vue
@@ -25,14 +25,20 @@
         </u-dropdown>
       </view>
     </view>
-    <view class="detailOpinion">
-      <u-input
-        v-model="detailOpinion"
-        type="textarea"
-        placeholder="请详细描述您的意见..."
-        :auto-height="true"
-        height="550"
-      />
+    <view class="box">
+      <scroll-view
+        class="detailOpinion"
+        :style="{ height: navHeight + 'px' }"
+        scroll-y="false"
+        scroll-x="false"
+      >
+        <u-input
+          v-model="detailOpinion"
+          type="textarea"
+          placeholder="请详细描述您的意见..."
+          :auto-height="true"
+        />
+      </scroll-view>
     </view>
     <view class="btn">
       <u-button>拒绝</u-button>
@@ -89,14 +95,27 @@ export default {
         },
       ],
       detailOpinion: '',//详细意见
+      pH: 0, //窗口高度
+      navHeight: 0, //元素的所需高度
 
     };
   },
   onLoad (option) {
     this.data = JSON.parse(decodeURIComponent(option.item));
   },
-
-
+  onReady () {//每次刷新页面获取一次高度
+    let that = this;
+    uni.getSystemInfo({ //调用uni-app接口获取屏幕高度
+      success (res) { //成功回调函数
+        that.pH = res.windowHeight //windoHeight为窗口高度
+        let scrollHeight = uni.createSelectorQuery().select(".detailOpinion"); //获取scrollHeight
+        scrollHeight.boundingClientRect(data => {
+          let pH = that.pH;
+          that.navHeight = pH - data.top - 90  //计算高度：元素高度=窗口高度-元素距离顶部的距离-底部btn的高度-margin-padding
+        }).exec()
+      }
+    })
+  },
   mounted () { },
 
   computed: {},
@@ -115,11 +134,16 @@ export default {
 
 </script>
 <style lang="scss">
+.details-container {
+  /deep/.u-dropdown__content__mask {
+    background-color: transparent;
+  }
+}
 </style>
 <style lang='scss' scoped>
 .details-container {
   width: 100%;
-  height: 100vh;
+  height: 100%;
   padding: 20rpx 0;
   .common {
     padding: 20rpx;
@@ -142,14 +166,22 @@ export default {
       }
     }
   }
-  .detailOpinion {
-    background-color: #fff;
-    padding: 20rpx;
+  .box {
+    overflow-x: hidden;
+    .detailOpinion {
+      background-color: #fff;
+      padding: 20rpx;
+    }
   }
   .btn {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    z-index: 999;
     display: flex;
-    justify-content: flex-end;
     margin-top: 10rpx;
+    height: 80rpx;
+    text-align: center;
     .u-btn--default {
       background-color: $default-color;
       color: #fff;
